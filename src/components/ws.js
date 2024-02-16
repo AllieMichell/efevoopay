733eabbf3f65e658d3f75ef2e1ba4100e00fb37d
AllieMichell <allie_michell@hotmail.com>
2024-02-16 06:20:49 -0600

    ADD TABLE

--- src/components/ws.js
+++ src/components/ws.js
@@ -4,6 +4,8 @@ const WebSocketComponent = () => {
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
   const [socket, setSocket] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
 
   useEffect(() => {
     const newSocket = new WebSocket('wss://wssx.gntapi.com:443'); // Change the URL to your WebSocket server
@@ -13,8 +15,11 @@ const WebSocketComponent = () => {
     };
 
     newSocket.onmessage = (event) => {
-      const newMessage = event.data;
-      console.log('newMessage', newMessage)
+      let newMessage = event.data;
+    //   console.log('newMessage',  JSON.parse(newMessage))
+    if(typeof newMessage === 'string'){
+        newMessage = JSON.parse(newMessage)
+    }
       setMessages((prevMessages) => [...prevMessages, newMessage]);
     };
 
@@ -35,7 +40,17 @@ const WebSocketComponent = () => {
       socket.send(inputMessage);
       setInputMessage('');
     }
-  };
+  }; 
+
+
+  // Calculate the index of the first and last item of the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  // Slice the array of messages to display only the messages for the current page
+  const currentMessages = messages.slice(0, 10);
+
+  // Function to handle pagination button clicks
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div>
@@ -47,11 +62,40 @@ const WebSocketComponent = () => {
         placeholder="Type your message..."
       />
       <button onClick={sendMessage}>Send</button>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index}>{message}</li>
+
+      <div>
+      <h2>Forex Prices</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Currency Pair</th>
+            <th>Bid Price</th>
+            <th>Ask Price</th>
+          </tr>
+        </thead>
+        <tbody>
+            {currentMessages.map((message, index) => 
+             ( 
+                    Object.entries(message.prices).map(([currencyPair, { bid, ask }]) => (
+                        <tr key={currencyPair}>
+                          <td>{currencyPair}</td>
+                          <td>{bid}</td>
+                          <td>{ask}</td>
+                        </tr>
+                      ))
+                )
+            )}
+        </tbody>
+      </table>
+    </div>
+     {/* Pagination buttons */}
+     <div>
+        {Array.from({ length: Math.ceil(messages.length / itemsPerPage) }).map((_, index) => (
+          <button key={index} onClick={() => paginate(index + 1)}>
+            {index + 1}
+          </button>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
